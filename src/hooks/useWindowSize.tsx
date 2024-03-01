27497a1480d187a65c1209a49a50512ea0a7a0d2
Saki Reid <saki.reid@gmail.com>
2024-03-01 20:14:31 +0100

    check if windown undefined

--- src/hooks/useWindowSize.tsx
+++ src/hooks/useWindowSize.tsx
@@ -1,13 +1,20 @@
 import { useState, useEffect } from "react";
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
+  if (typeof window !== "undefined") {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  } else {
+    // Return default dimensions or handle the case where window is not available
+    return {
+      width: 0,
+      height: 0,
+    };
+  }
 }
-
 export default function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState(
     getWindowDimensions()
